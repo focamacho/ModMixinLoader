2f524b3100efe6778f835e1707b7d88bc2b60ad6
focamacho <renancolombomail@gmail.com>
2023-06-16 14:24:42 -0300

    feat: add a mod main class, so it can be used as dependency by other mods

---
+++ src/main/java/com/focamacho/modmixinloader/Main.java
@@ -0,0 +1,12 @@
+package com.focamacho.modmixinloader;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Main.MOD_ID, name = Main.NAME, version = Main.VERSION, acceptableRemoteVersions = "*")
+public class Main {
+
+    public static final String MOD_ID = "modmixinloader";
+    public static final String NAME = "ModMixinLoader";
+    public static final String VERSION = "1.1";
+
+}
