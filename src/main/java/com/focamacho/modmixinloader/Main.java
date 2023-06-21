53284ad379ae6cdd2b9793bd139464386c4e1943
focamacho <renancolombomail@gmail.com>
2023-06-21 11:57:07 -0300

    fix: wrong version

--- src/main/java/com/focamacho/modmixinloader/Main.java
+++ src/main/java/com/focamacho/modmixinloader/Main.java
@@ -7,6 +7,6 @@ public class Main {
 
     public static final String MOD_ID = "modmixinloader";
     public static final String NAME = "ModMixinLoader";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.0.2";
 
 }
